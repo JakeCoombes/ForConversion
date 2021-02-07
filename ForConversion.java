b5308e1fdfbdcc8267e290561b8bc96ae3b3b23c
JakeCoombes <78572836+JakeCoombes@users.noreply.github.com>
2021-02-07 16:33:15 -0500

    Add files via upload

---
+++ ForConversion.java
@@ -0,0 +1,23 @@
+/* Name:Jake Coombes
+ * Date:10/30/18
+ * Description:
+ * 		create a for loop that goes from 0 to 50 
+ * 		and prints out all the numbers except 50
+ */
+
+package lab7part3;
+
+public class ForConversion {
+
+	public static void main(String[] args) {
+		
+		int loopVariable = 0;
+		
+		for(loopVariable = 0;loopVariable < 50; loopVariable++)
+		{
+			System.out.println("Loop Iteration # " + loopVariable );
+		}
+
+	}
+
+}
